58335d5fada2192553fa641f78df8b58ebec2748
sindhuk98 <12sinz@gmail.com>
2020-09-22 12:21:31 -0400

    added yearly and monthly amortization tables

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const inputInt = document.querySelector("#interest")
 const inputYrs = document.querySelector("#years")
 const principal = document.getElementById("calc")
 const startDate = document.getElementById("startDate")
+const AnnualTable = document.getElementById("AnnualTableBody")
+const MonthlyTable = document.getElementById("MonthlyTableBody")
+var calcArray=[]
 var flag = true
 
 console.log(inputDP.value)
@@ -49,7 +52,7 @@ function callback(e) {
     document.getElementById("submit1").disabled=true
     const interest = parseFloat(inputInt.value)/100/12;
     const months = parseInt(inputYrs.value)*12
-    const principalValue = parseFloat(principal.value)
+    var principalValue = parseFloat(principal.value)
 
     const EMI = Math.round((principalValue * interest * (1 + interest)**months)/(((1+interest)**months)-1)*100)/100
     document.getElementById('EMI').textContent = "EMI: " + formatNumber(EMI);
@@ -59,23 +62,69 @@ function callback(e) {
     document.getElementById('totalPropValue').textContent += formatNumber(input.value)
     document.getElementById('totalPrinValue').textContent += formatNumber(principal.value)
     document.getElementById('totalInterestRate').textContent += formatNumber(inputInt.value) + "%"
-    document.getElementById('totalTerm').textContent += formatNumber(inputYrs.value)
+    document.getElementById('totalTerm').textContent += formatNumber(inputYrs.value) + "years"
     document.getElementById('totalStartDate').textContent += startDate.value
     var endDate = new Date(startDate.value)
     endDate.setFullYear(endDate.getFullYear() + parseInt(inputYrs.value))
     const endDateStr = endDate.toISOString()
     document.getElementById('totalEndDate').textContent += endDateStr.substring(0,endDateStr.length-14)
     document.getElementById('totalInterestPayments').textContent += formatNumber(Math.round(((EMI * months) - principalValue)*100)/100)
-    document.getElementById('totalPayments').textContent += formatNumber(EMI * months)
+    document.getElementById('totalPayments').textContent += formatNumber(Math.round(EMI * months*100)/100)
 
-    
-
+    for(i=0; i<months;i++){
+        var item=[];
+        const interestPayment = Math.round(principalValue*interest*100)/100
+        item.push(interestPayment)
+        item.push(Math.round((EMI-interestPayment)*100)/100)
+        item.push(principalValue-EMI+interestPayment)
+        principalValue = principalValue - EMI + interestPayment
+        calcArray.push(item)
+    }    
 }
 
 function resetForm() {
     document.getElementById('calcMenu').style.display="none"
     document.getElementById('EMI').textContent = ""
     document.getElementById('form').reset()
+}
+
+function AnnualTableCalc() {
+    var rows = parseInt(inputYrs.value);
+    var dataHtml = ''
+    const date = new Date(startDate.value)
+    console.log(date.toISOString().substr(0,4))
+    var year = parseInt(date.toISOString().substr(0,4))
+    var remainingMonths = parseInt(date.toISOString().substr(5,2))
+    var principalRemaining = principal.value
+    for (var r = 0; r < rows; r++) {
+        var interestPortion=0;
+        var principalPortion=0;
+        dataHtml+='<tr>'
+        for(var i=0;i<12;i++){
+            interestPortion += Math.round(calcArray[(r*12)+i][0]*100)/100
+            principalPortion += calcArray[(r*12)+i][1]
+        }
+            dataHtml+='<td>' + year + '</td>' + '<td>' + Math.round(interestPortion*100)/100 + '</td>' + '<td>'+ Math.round(principalPortion*100)/100+'</td>' + '<td>'+Math.round(calcArray[(r*12)+11][2]*100)/100+'</td>'
+        year++;
+        dataHtml+='</tr>'
+    }
+    AnnualTable.innerHTML=dataHtml
+}
+
+function MonthlyTableCalc() {
+    var rows = parseInt(inputYrs.value);
+    var dataHtml = ''
+    const date = new Date(startDate.value)
+    var year = 1
+    var remainingMonths = parseInt(date.toISOString().substr(5,2))
+    var principalRemaining = principal.value
+    for (var r = 0; r < rows*12; r++) {
+        dataHtml+='<tr>'
+            dataHtml+='<td>' + year + '</td>' + '<td>' + calcArray[r][0] + '</td>' + '<td>'+calcArray[r][1]+'</td>' + '<td>'+Math.round(calcArray[r][2]*100)/100+'</td>'
+        year++;
+        dataHtml+='</tr>'
+    }
+    MonthlyTable.innerHTML=dataHtml
 }
 
 /*
